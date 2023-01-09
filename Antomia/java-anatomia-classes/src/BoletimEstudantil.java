eb7cbe22bad3dec5423734de084bad39e2b8ae81
jpmmat <jpmmat@hotmail.com>
2023-01-09 11:57:50 -0300

    3 commit

---
+++ Antomia/java-anatomia-classes/src/BoletimEstudantil.java
@@ -0,0 +1,14 @@
+import javax.sound.midi.Soundbank;
+
+public class BoletimEstudantil{
+
+    public static void main(String[] args) {
+        int mediaFinal = 5;
+        if(mediaFinal<6)
+            System.out.println("REPROVADO");
+        else if (mediaFinal == 6)
+            System.out.println(" PROVA DE MINERVA");
+        else 
+            System.out.println("APROVADO");
+    }
+}
--- Antomia/java-anatomia-classes/src/MinhaClasse.java
+++ Antomia/java-anatomia-classes/src/MinhaClasse.java
@@ -3,6 +3,7 @@ public class MinhaClasse {
     public static void main(String[] args) {
         System.out.println("Começando Novo bootcamp");
     
+
     }
     
 
